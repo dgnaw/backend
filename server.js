c779a62a6abfc93eaf24cdd758d28e4ee4105ee9
dgnaw <dogianam09072005@gmail.com>
2025-04-28 09:14:20 +0700

    init commit

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const cors = require('cors');  // Import the CORS package
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Enable CORS for all origins (or specify your frontend URL for more security)
+app.use(cors({
+  origin: 'https://frontend-git-dgnaw-dev.apps.rm3.7wse.p1.openshiftapps.com', // Set your frontend URL here
+}));
 
 // Route API
 app.get('/api', (req, res) => {
